2f9dafc8e6ab022b35afa1550579054f65d97e15
swasanaweddingpusat <swasanaweddingpusat@gmail.com>
2026-02-06 18:24:32 +0700

    testimonials client section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,6 +2,7 @@ import { Layout } from '@/components/layout/Layout';
 import { HeroSection } from '@/components/home/HeroSection';
 import { FeaturesSection } from '@/components/home/FeaturesSection';
 import { ProgramsSection } from '@/components/home/ProgramsSection';
+import { TestimonialsSection } from '@/components/home/TestimonialsSection';
 import { CTASection } from '@/components/home/CTASection';
 
 const Index = () => {
@@ -10,6 +11,7 @@ const Index = () => {
       <HeroSection />
       <FeaturesSection />
       <ProgramsSection />
+      <TestimonialsSection />
       <CTASection />
     </Layout>
   );
